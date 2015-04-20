aae650e7f00326ce53198292f4355bbbe2ba07df
Sulman Baig <sulman1990@hotmail.com>
2015-04-21 00:12:09 +0200

    generated gallery scaffold and updated relation to user

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,8 @@
 class PagesController < ApplicationController
   def home
+    if current_user
+      redirect_to galleries_path
+    end
   end
 
   def about
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150420215552) do
+ActiveRecord::Schema.define(version: 20150420220423) do
+
+  create_table "galleries", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
